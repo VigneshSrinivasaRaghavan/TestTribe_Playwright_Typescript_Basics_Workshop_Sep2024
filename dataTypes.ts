7553b5e20185324778926e8db655c13bbf1bc8cf
Vignesh Srinivasa Raghavan <40608099+VigneshSrinivasaRaghavan@users.noreply.github.com>
2024-09-20 14:45:49 +0200

    Added Variables, DataTypes Test

---
+++ dataTypes.ts
@@ -0,0 +1,21 @@
+// Number
+var vigneshAge = 30;
+var piValue = 3.14244;
+
+// String
+var trainerName = "Vignesh";
+var initialName = 'S';
+
+// Boolean
+var doesStudentsLikingVignesh = true;
+var doesStudentsFeelingBored = false;
+
+// Any
+var dynamicValue:any = 42;
+dynamicValue = "Vignesh";
+dynamicValue = false;
+
+// Null and Undefined
+var nullVariable:null = null;
+var undefinedValue = undefined;
+undefinedValue = 32;
---
+++ variable.ts
@@ -0,0 +1,18 @@
+// 3 keywords
+// Syntax to create a variable
+// variableKeyword variableName = value;
+
+var studentName = "Vignesh";
+let studentAge = 30;
+const studentGender = "Male";
+
+console.log(studentName);
+console.log(studentAge);
+console.log(studentGender);
+
+studentName = "Vignesh Srinivasa Raghavan";
+studentAge = 31;
+// studentGender = "Female";
+
+console.log(studentName);
+console.log(studentAge);
